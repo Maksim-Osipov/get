9e40e038d5134d70c3b0da8da65659aaef4ba552
Maksim-Osipov <max26012007@gmail.com>
2025-09-16 15:50:04 +0300

    digital-switch

--- digital-blink.py
+++ digital-blink.py
@@ -1,10 +1,16 @@
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode(GPIO.BCM)
+
 led = 26
+
 GPIO.setup(led, GPIO.OUT)
-stats = 0
-peroid = 1.0
+
+state = 0
+
+period = 1.0
+
 while True:
     GPIO.output(led, state)
     state = not state
